e61c89edd708b2444287d16fce9005c525820e85
Tanushree Nerella <tanushreenerella697@gmail.com>
2026-03-06 12:47:28 +0530

    socket changes

--- backend/app.py
+++ backend/app.py
@@ -47,4 +47,4 @@ import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=False)
+    socketio.run(app, host="0.0.0.0", port=port, debug=False, allow_unsafe_werkzeug=True)
